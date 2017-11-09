5ba26bd55ddf6e8a6b7e6003a80227e2cef143d8
ryou62525 <majaj62525@gmail.com>
2017-11-09 17:28:44 +0900

    アップしなおしました

---
+++ HoloProject/Assets/Game/Scripts/SetNear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNear : MonoBehaviour
+{
+    private float _nearVal = 0.1f;
+    private Camera _camera = null;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        if (!_camera) Debug.Log("カメラがセットされていません");
+    }
+
+    public void OnClickedUp()
+    {
+        if (_camera != null)
+        {
+            _camera.nearClipPlane += _nearVal;
+        }
+    }
+
+    public void OnClickedDown()
+    {
+        if (_camera != null)
+        {
+            _camera.nearClipPlane -= _nearVal;
+        }
+    }
+
+}
